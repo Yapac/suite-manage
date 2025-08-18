7a27b6db78fe419ffe6106278915ddfc1fdeff76
Yassine El atlassi <yassinelatlassi@gmail.com>
2025-08-18 23:00:43 +0100

    Finalizing full stack structure

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-import "dotenv/config";                  // replaces require('dotenv').config()
-import express from "express";           // replaces require('express')
+import "dotenv/config"; // replaces require('dotenv').config()
+import express from "express"; // replaces require('express')
 import { ApolloServer } from "apollo-server-express";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import typeDefs from "./schema/typeDefs.js";       // use .js extension in ES modules
+import typeDefs from "./schema/typeDefs.js"; // use .js extension in ES modules
 import resolvers from "./schema/resolvers.js";
 
 async function startServer() {
@@ -25,7 +25,9 @@ async function startServer() {
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () =>
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+    console.log(
+      `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+    )
   );
 }
 
