49f5db1f823ed28eddf09986464f2e790fd7ad7a
Yassine El atlassi <yassinelatlassi@gmail.com>
2025-09-13 16:10:37 +0100

    Adding the logo to the header in mobile

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,25 +1,46 @@
 import React from "react";
 import { CurrentUser } from "./current-user";
-import { Button, Layout, Space, theme as antdTheme } from "antd";
+import { Button, Layout, Space, theme as antdTheme, Grid } from "antd";
 import { MoonOutlined, SunOutlined } from "@ant-design/icons";
 import { useAppContext } from "../../context/AppContext";
 
 const Header = () => {
   const { token } = antdTheme.useToken();
+  const { state, dispatch } = useAppContext();
+
+  const { useBreakpoint } = Grid;
+  const screens = useBreakpoint();
+
   const headerStyles: React.CSSProperties = {
     background: token.colorBgContainer,
     display: "flex",
-    justifyContent: "flex-end",
+    justifyContent: "space-between",
     alignItems: "center",
     padding: "0 24px",
     position: "sticky",
     top: 0,
     zIndex: 999,
   };
-  const { state, dispatch } = useAppContext();
 
   return (
     <Layout.Header style={headerStyles}>
+      <Space align="center" size="middle">
+        {!screens.md && (
+          <div
+            style={{ display: "flex", alignItems: "center", columnGap: "8px" }}
+          >
+            <img src="hotel-bell.png" style={{ width: "23px" }} alt="Logo" />
+            <p
+              style={{
+                margin: "0",
+                fontWeight: "bold",
+              }}
+            >
+              Suite Manage
+            </p>
+          </div>
+        )}
+      </Space>
       <Space align="center" size="middle">
         <Button
           onClick={() => dispatch({ type: "TOGGLE_THEME" })}
